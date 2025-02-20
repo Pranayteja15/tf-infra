af208763654abbbd5dfb9478a15f34b166f2108a
pranay <moolipranayreddy@presidio.com>
2025-02-20 15:26:04 +0530

    updated resources in main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-    bucket         = "my-terraform-state-bucket-latest"
+    bucket         = "pt-tf-jenkins"
     key    = "terraform/jenkins/statefile.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-lock"
